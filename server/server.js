6ba337f6996ff84fdca4e55d11b8f75b7f5f8a9f
Ismael Agchar <agcharis67705@th-nuernberg.de>
2019-10-17 16:14:24 +0200

    Add node server script

---
+++ server/server.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const express = require('express')
+const app = express()
+const port = 80;
+
+const MongoClient = require('mongodb').MongoClient
+const url = 'mongodb://paredros-creator-db:27017'
+const dbName = 'paredrosCreatorDb'
+const collName = 'adventures'
+
+app.get('/getTestAdventure', (req, res) => {
+    MongoClient.connect(url, {useNewUrlParser: true})
+    .then(client => {
+        let db = client.db(dbName)
+        let coll = db.collection(collName)
+        coll.find({})
+        .toArray()
+        .then(adventure => {
+            client.close()
+            res.status(200).send(adventure)
+        })
+    })
+    .catch(err => res.status(400).send('failed: ' + err))
+})
+
+app.listen(port, () => {
+    console.log('paredros creator running on port ' + port)
+})
