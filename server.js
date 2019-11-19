b745fd309be1cef7318d5b5538440fd78de35f37
Ismael Agchar <agcharis67705@th-nuernberg.de>
2019-11-19 18:56:17 +0100

    reset server port to 80

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const port = 3000;
+const port = 80;
 app.use(express.json())
 app.use(express.static('public'))
 app.use(cors({ credentials: true }))
