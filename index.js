f2103a4d57a9bbcfa30e7eed200213d6d92d1ccf
Fabian Meister <fabianmeister@Fabians-MacBook-Pro.local>
2019-11-28 10:29:27 +0100

    port fix

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var fs = require('fs'),
 var app = require('connect')();
 var swaggerTools = require('swagger-tools');
 var jsyaml = require('js-yaml');
-var serverPort = 8081;
+var serverPort = 80;
 
 //static serve 
 var serveStatic = require('serve-static')
