24d475f4cc79dcedf35ce5ee30a830faf98081ea
Fabian Meister <fabianmeister@Fabians-MBP.fritz.box>
2019-11-29 23:16:06 +0100

    removed test

--- test/test.js
+++ test/test.js
@@ -3,25 +3,3 @@
 
 const now = new Date()
 console.log(Date() + ': the testing for the paredros api has not been implemented yet and hence will pass')
-
-const chai = require('chai');
-const expect = require('chai').expect;
-
-chai.use(require('chai-http'));
-
-const app = require('../index.js'); // Our app
-
-describe('API endpoint /api/adventures', function() {
-  this.timeout(250000); // How long to wait for a response (ms)
-
-  // GET ALL Adverntures that fails because of a bad token
-  it('Get All Adventures', function() {
-    return chai.request(app)
-      .get('/api/adventures')
-      .set('token', 'thisIsNotASeriousToken')
-      .then(function(res) {
-        expect(res).to.have.status(401);
-      });
-  });
-
-});
